28a0fdc352649641cf6048f3944c067e9a026cd1
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2017-10-12 00:30:48 -0700

    Added method to convert protobuf to JSON

--- src/main/java/org/mule/modules/grpc/GRPCConnector.java
+++ src/main/java/org/mule/modules/grpc/GRPCConnector.java
@@ -2,17 +2,21 @@ package org.mule.modules.grpc;
 
 import java.util.Map;
 
+import org.mule.api.MuleMessage;
 import org.mule.api.annotations.Config;
 import org.mule.api.annotations.Connector;
 import org.mule.api.annotations.MetaDataScope;
 import org.mule.api.annotations.Processor;
 import org.mule.api.annotations.param.Default;
 import org.mule.api.annotations.param.MetaDataKeyParam;
-import org.mule.api.annotations.param.Payload;
+import org.mule.api.transformer.TransformerException;
 import org.mule.modules.grpc.config.GRPCConfiguration;
 import org.mule.modules.grpc.util.GRPCReflectionUtil;
+import org.mule.transformer.types.SimpleDataType;
 
 import com.google.protobuf.AbstractMessage;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 
 import io.grpc.stub.AbstractStub;
 
@@ -32,6 +36,12 @@ public class GRPCConnector {
         AbstractStub<?> stub = config.getBlockingStub();
     	AbstractMessage message = GRPCReflectionUtil.buildOperationArgument(stub.getClass(), operation, data);
     	return GRPCReflectionUtil.invokeInStub(operation, message, config.getBlockingStub());
+    }
+    
+    @Processor
+    public void convertMessageToJson(MuleMessage mmsg) throws InvalidProtocolBufferException, TransformerException {
+    	 String payload = JsonFormat.printer().print(mmsg.getPayload(AbstractMessage.class));
+    	 mmsg.setPayload(payload, new SimpleDataType<String>(java.lang.String.class, "application/json"));
     }
 
     public GRPCConfiguration getConfig() {
